a63baa60688ea12200b26c0246b4f0f85742ddcd
bisthebis <bism25@hotmail.com>
2016-11-13 15:01:46 +0100

    Version information in initGL

--- src/app/samplewidget.cpp
+++ src/app/samplewidget.cpp
@@ -4,6 +4,8 @@
 #include <QOpenGLContext>
 #include <QOpenGLFunctions>
 
+
+
 SampleWidget::SampleWidget(QWidget *parent, Qt::WindowFlags f) : QOpenGLWidget(parent, f)
 {
 
@@ -13,6 +15,13 @@ void SampleWidget::initializeGL()
 {
     QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
     f->glClearColor(0, 0.5, 1, 1);
+    auto context = QOpenGLContext::currentContext();
+    qDebug() << QString("OpenGL Informations : GLES ? %0. Major version : %1. Minor version : %2")
+                .arg
+                (context->isOpenGLES())
+                .arg
+                (context->format().version().first)
+                .arg(context->format().version().second);
 }
 void SampleWidget::resizeGL(int w, int h)
 {
